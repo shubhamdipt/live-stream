9b8e1aaafa69cb55def278f0245c06f9b441ca43
Shubham Dipt <shubham.dipt@gmail.com>
2020-01-12 16:15:28 +0100

    Live and historical streaming separated

--- routes/pages.py
+++ routes/pages.py
@@ -10,12 +10,28 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/video_feed')
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
+
+@app.route('/feed/live_video_feed')
+@basic_auth.required
+def live_video_feed():
+    def gen(camera):
+        while True:
+            time.sleep(0.2)
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + camera.get_live_frame() + b'\r\n')
+    return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/feed/video_feed')
 @basic_auth.required
 def video_feed():
     def gen(camera):
         while True:
-            time.sleep(0.2)
+            # time.sleep(0.2)
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + camera.get_frame() + b'\r\n')
     return Response(gen(Camera()), mimetype='multipart/x-mixed-replace; boundary=frame')
--- routes/utils.py
+++ routes/utils.py
@@ -3,8 +3,10 @@ from application import IMAGE_PATH
 import os
 import re
 
-TIME_ZONE_HOURS_DIFFERENCE = 2
+TIME_ZONE_HOURS_DIFFERENCE = 0
+TILL_HOUR = 3
 TIME_FORMAT = "%Y%m%d%H%M%S"
+IMAGE_FORMAT = "jpg"
 
 
 def get_sorted_files(
@@ -17,7 +19,7 @@ def get_sorted_files(
     else:
         latest_time -= timedelta(hours=TIME_ZONE_HOURS_DIFFERENCE)
 
-    files_to_evaluate = list()
+    files_to_evaluate = []
     for f in os.listdir(src_dir):
         try:
             creation_time = datetime.strptime(f.split(".")[0], TIME_FORMAT)
@@ -42,16 +44,43 @@ def get_sorted_files(
 class Camera:
 
     def __init__(self):
-        self.frames = self.get_all_frames()
+        # self.frames = self.get_all_frames()
+        self.current_time = datetime.now()
         self.last_frame_number = -1
+        self.latest_time = self.current_time - timedelta(hours=TIME_ZONE_HOURS_DIFFERENCE+TILL_HOUR)
+        self.last_image = None
+
+    def get_live_frame(self):
+        curr_time = datetime.now()
+        while True:
+            try:
+                image_file = open(
+                    os.path.join(
+                        IMAGE_PATH, "{}.{}".format(
+                            curr_time.strftime(TIME_FORMAT),
+                            IMAGE_FORMAT)), 'rb').read()
+                self.last_image = image_file
+                return image_file
+            except FileNotFoundError:
+                curr_time -= timedelta(seconds=1)
+                if self.last_image:
+                    return self.last_image
+            except Exception as e:
+                return open("loading.jpg", "rb").read()
 
     def get_frame(self):
-        if len(self.frames) == 0:
-            return open("blank.png", "rb").read()
-        self.last_frame_number += 1
-        if self.last_frame_number >= len(self.frames):
-            self.last_frame_number = 0
-        return self.frames[self.last_frame_number]
+        self.current_time -= timedelta(seconds=1)
+        while self.current_time > self.latest_time:
+            try:
+                image_file = open(
+                    os.path.join(
+                        IMAGE_PATH, "{}.{}".format(
+                            self.current_time.strftime(TIME_FORMAT),
+                            IMAGE_FORMAT)), 'rb').read()
+                return image_file
+            except FileNotFoundError:
+                self.current_time -= timedelta(seconds=1)
+        return open("loading.jpg", "rb").read()
 
     @staticmethod
     def get_all_frames():
